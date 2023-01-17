6cdbc19ae662f86cfa65ee264d74a7ffe010155e
Avinash B <54209731+ragingbull1210@users.noreply.github.com>
2023-01-18 02:01:21 +1300

    implement product api controller

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ProductsController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ProductsController(DataContext context)
+        {
+            _context = context; 
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetActivities()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetActivity(Guid id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
