56a97cbdfa924027afe650e1e57d93c9326ce80c
Avinash B <54209731+ragingbull1210@users.noreply.github.com>
2023-01-18 03:38:26 +1300

    basic products table

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+import { Table, TableCell, TableHead, TableRow } from "@mui/material";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import TableBody from "@mui/material/TableBody";
+import Container from "@mui/material/Container";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -15,15 +19,33 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <ul>
-          {products.map((product: any) => (
-            <li key={product.id}>{product.name}</li>
-          ))}
-        </ul>
-      </header>
+      <Container>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simpe table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Price</TableCell>
+                <TableCell>Type</TableCell>
+                <TableCell>Active</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products.map((product: any) => (
+                <TableRow
+                  key={product.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell>{product.name}</TableCell>
+                  <TableCell>${product.price.toFixed(2)}</TableCell>
+                  <TableCell>{product.type}</TableCell>
+                  <TableCell>{product.active.toString()}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
     </div>
   );
 }
