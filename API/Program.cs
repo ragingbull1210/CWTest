679451c5f91480ff89874f3dd1d39d4989365ef6
Avinash B <54209731+ragingbull1210@users.noreply.github.com>
2023-01-18 02:55:03 +1300

    fix incorrect http string format

--- API/Program.cs
+++ API/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
     {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http:localhost:3000");
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
     });
 });
 
