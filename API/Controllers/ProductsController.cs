fc0c2213b0f717f478b875d3f764ba3125984b8f
Avinash B <54209731+ragingbull1210@users.noreply.github.com>
2023-01-18 15:53:24 +1300

    delete handler

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -30,5 +30,11 @@ namespace API.Controllers
             product.Id = id;
             return Ok(await Mediator.Send(new Edit.Command { Product = product }));
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+        }
     }
 }
